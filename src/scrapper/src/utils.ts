668565588719ead124390a2e273f741a1c7f7b3c
Tano Abeleyra <tano@abeleyra.com>
2020-08-04 22:50:33 -0300

    Add one more ascii code

--- src/scrapper/src/utils.ts
+++ src/scrapper/src/utils.ts
@@ -2,7 +2,7 @@ export const dateToTimestamp = (date: string | Date): number =>
   new Date(date).getTime();
 
 export const asciiToChar = (text: string): string => {
-  const codes = [8211, 8220, 8221];
+  const codes = [8211, 8217, 8220, 8221];
   codes.forEach((code) => {
     text = text.replace(`&#${code};`, String.fromCharCode(code));
   });
